28c33bf39b96fe113d44f12ba2e666c001ca9eea
James Tolen <jtolen1@my.bcit.ca>
2025-11-02 22:22:55 -0800

    Changed url

--- assets/js/config.js
+++ assets/js/config.js
@@ -3,6 +3,6 @@ export const Config = Object.freeze({
   // Set this to your backend API origin (no trailing slash)
 // apiBaseUrl: 'https://comp4537serverside-bcfgfrhxash8fkhv.canadacentral-01.azurewebsites.net/api'
   // apiBaseUrl: 'http://localhost:3000/api'
-  apiBaseUrl: 'https://comp4537projectserverside.onrender.com/api'
+  apiBaseUrl: 'https://comp4537serverside.onrender.com/api'
 });
 // moved to client/
